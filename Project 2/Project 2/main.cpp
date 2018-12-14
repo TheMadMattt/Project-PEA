4cb957ac41462b6dc86c8745e29067156f4785b9
Mateusz Polok <matipolok@gmail.com>
2018-12-14 21:27:09 +0100

    ---

--- Project 2/Project 2/main.cpp
+++ Project 2/Project 2/main.cpp
@@ -9,26 +9,6 @@ int main()
 {
 	srand(time(nullptr));
 
-	/*double coolTemp = 0.9999, finalTemp = 0.0001;
-	int stopTime = 0, randOption = 1, startOption = 1;
-
-	result results;
-	Graph graph;
-	SimulatedAnnealing sa;
-	int size = 10;
-	vector<result> vect;
-
-	graph.readFromFile("ftv47.atsp");
-	for(int i=0; i < size; i++)
-	{
-		sa = SimulatedAnnealing(coolTemp, finalTemp, graph.getMatrix(), stopTime, randOption, startOption);
-		results = sa.find_solution(0, 1, 1);
-		vect.push_back(results);
-		cout << results.cost << endl;
-	}
-	TimeMeasure::saveToFile(vect, "ftv47time.txt");*/
-
-
 	Menu menu;
 
 	menu.ChooseOptionMenu();
